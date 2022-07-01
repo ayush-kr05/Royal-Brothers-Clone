10a8ea122307cb48f92cad8850bc7612573f6d41
DevAkram <57341544+Akram3598786@users.noreply.github.com>
2022-07-01 15:00:12 +0530

    Tariff page created

--- tarrif/tariff.js
+++ tarrif/tariff.js
@@ -34,6 +34,78 @@ let availble_bikes=[
         km_limit: 120.0,
         id:3
       },
+      {
+        image_url:
+        "https://d3vp2rl7047vsp.cloudfront.net/bike_models/images/000/000/233/medium/Honda_activa_5G.png?1532687566",
+        name:"Honda Activa 5G",
+        row1_price: 650,
+        row2_price: 750,
+        available:"Daily",
+        excess_km: 4.0,
+        km_limit :120.0,
+        id:5
+    
+    },
+    {
+        image_url:
+        "https://d3vp2rl7047vsp.cloudfront.net/bike_models/images/000/000/257/medium/ntorq2.png?1571407789",
+        name:"TVS Ntorq 125",
+        row1_price: 650,
+        row2_price: 750,
+        available:"Daily",
+        excess_km: 4.0,
+        km_limit :120.0,
+        id:6
+    
+    },
+    {
+        image_url:
+        "https://d3vp2rl7047vsp.cloudfront.net/bike_models/images/000/000/016/medium/Bajaj_Pulsar-min_2.png?1519738545",
+        name:"Bajaj Pulsar 150",
+        row1_price: 650,
+        row2_price: 750,
+        available:"Daily",
+        excess_km: 4.0,
+        km_limit :120.0,
+        id:7
+    
+    },
+    {
+        image_url:
+        "https://d3vp2rl7047vsp.cloudfront.net/bike_models/images/000/000/020/medium/FZ_Version_1.png?1504100292",
+        name:"Yamaha FZ",
+        row1_price: 650,
+        row2_price: 750,
+        available:"Daily",
+        excess_km: 4.0,
+        km_limit: 120.0,
+        id:8
+    
+    },
+    {
+        image_url:
+        "https://d3vp2rl7047vsp.cloudfront.net/bike_models/images/000/000/265/medium/Glamour_125.png?1571327418",
+        name:"Hero Glamour",
+        row1_price: 650,
+        row2_price: 750,
+        available:"Daily",
+        excess_km: 4.0,
+        km_limit: 120.0,
+        id:9
+    
+    },
+    {
+        image_url:
+        "https://d3vp2rl7047vsp.cloudfront.net/bike_models/images/000/000/014/medium/Himalayan_RE-min_2.png?1519739226",
+        name:"Royal Enfield Himalayan",
+        row1_price: 650,
+        row2_price: 750,
+        available:"Daily",
+        excess_km: 4.0,
+        km_limit: 120.0,
+        id:10
+    
+    },
    
 ];
 
@@ -54,37 +126,6 @@ let bike_incart=JSON.parse(localStorage.getItem("cart")) || [];
     })
     display_products(availble_bikes);
   }
- 
-
-/*
-
-  let row1_days=document.createElement("h4");
-  row1_days="Mon-Thu"
-  let row1_day_n_price=document.createElement("div");
-  let row1_day_sts = document.createElement("p");
-  row1_day_sts.innerText="Full day";
-  let row1_price = document.createElement("p");
-  row1_price.innerText=ele.row1_price;
-  row1_day_n_price.append(row1_day_sts,row1_price)
-  row1.append(row1_days,row1_day_n_price);
-
-
-  let row2_days=document.createElement("h4");
-  row2_days="Mon-Thu"
-  let row2_day_n_price=document.createElement("div");
-  let row2_day_sts = document.createElement("p");
-  row2_day_sts.innerText="Full day";
-  let row2_price = document.createElement("p");
-  row2_price.innerText=ele.row2_price;
-  row2_day_n_price.append(row2_day_sts,row1_price)
-  row2.append(row2_days,row2_day_n_price);
- 
-
-  let row3_heading=document.createElement("h4");
-  row3_heading.innerText="Excess charges";
-  let excess_km_div=document.createElement("div");
-  let km_limit_div=document.createElement("div");
-*/
 
   function display_products(data){
 
@@ -96,77 +137,98 @@ let bike_incart=JSON.parse(localStorage.getItem("cart")) || [];
       let name=document.createElement("h3");
        name.setAttribute("id","bike_name");
       name.innerText=ele.name;
-    document.createElement("img");
-    let img=document.getElementById("bike_img");
-     img.setAttribute("src",ele.image_url);
-    let availbilty_p = document.getElementById("availbilty_p");
+
+   let img= document.createElement("img");
+   img.setAttribute("src",ele.image_url);
+   img.setAttribute("id","bike_img");
+    
+     
+    let availbilty_p = document.createElement("p");
+    availbilty_p.setAttribute("id","availbilty_p");
     availbilty_p.innerText = ele.availble;
 
-    let price_details = document.getElementById("details");
+    let price_details = document.createElement("div");
+    price_details.setAttribute("id","details");
 
    //  ======= Row-1 Code ======
-    let row1_days=document.getElementById("row1-availblity");
+    let row1_days=document.createElement("h4");
+     row1_days.setAttribute("id","row1-availblity");
     row1_days.innerText="Mon-Thu";
-    let row1_day_n_price_div=document.getElementById("row1_day_n_price_div");
-    let row1_day_sts = document.querySelector("#row1_day_sts");
+
+    let row1_day_n_price_div=document.createElement("div");
+    row1_day_n_price_div.setAttribute("class","day_n_price ");
+
+     let row1_day_sts = document.createElement("p");
+     row1_day_sts.setAttribute("id","row1_day_sts");
     row1_day_sts.innerText="Full day";
-    let row1_price = document.querySelector("#row1_price");
+
+    let row1_price = document.createElement("p");
     row1_price.innerText="Rs. "+ele.row1_price;
     row1_day_n_price_div.append(row1_day_sts,row1_price);
 
   //  ======= Row-2 Code ======
-    let row2_days=document.getElementById("row2-availblity");
-    row2_days.innerText="Mon-Thu";
-    let row2_day_n_price_div=document.getElementById("row2_day_n_price_div");
-    let row2_day_sts = document.querySelector("#row2_day_sts");
-    row2_day_sts.innerText="Full day";
-    let row2_price = document.querySelector("#row2_price");
-    row2_price.innerText="Rs. "+ele.row2_price;
-    row2_day_n_price_div.append(row2_day_sts,row2_price);
+  let row2_days=document.createElement("h4");
+  row2_days.setAttribute("id","row2-availblity");
+ row2_days.innerText="Fri-Sun";
+
+ let row2_day_n_price_div=document.createElement("div");
+ row2_day_n_price_div.setAttribute("class","day_n_price")
+
+  let row2_day_sts = document.createElement("p");
+  row2_day_sts.setAttribute("id","row2_day_sts");
+ row2_day_sts.innerText="Full day";
+
+ let row2_price = document.createElement("p");
+ row2_price.innerText="Rs. "+ele.row2_price;
+ row2_day_n_price_div.append(row2_day_sts,row1_price);
 
   //  ======= Row-3 Code ======
-    let row3_heading=document.getElementById("row3-heading");
+    let row3_heading=document.createElement("h4");
+     row3_heading.setAttribute("id","row3-heading");
     row3_heading.innerText="Excess charges";
-    let row3_day_n_price_div = document.getElementById("row3_day_n_price_div");
-
-    let excess_chrges_keys = document.getElementById("excess_chrges_keys");
-    let excess_km_key = document.getElementById("excess_km_key");
+    let row3_day_n_price_div = document.createElement("div");
+  
+    let excess_chrges_keys = document.createElement("div");
+    excess_chrges_keys.setAttribute("class","excess_chrges")
+     excess_chrges_keys.setAttribute("id","excess_chrges_keys");
+    let excess_km_key = document.createElement("p");
     excess_km_key.innerText = "Excess km";
-    let km_limit_key = document.getElementById("km_limit_key");
+    let km_limit_key = document.createElement("p");
     km_limit_key.innerText = "km Limit";
     excess_chrges_keys.append(excess_km_key,km_limit_key);
 
-    let excess_chrges_value = document.getElementById("excess_chrges_value");
-    let excess_km_price = document.getElementById("excess_km_price");
+    let excess_chrges_value = document.createElement("div");
+    excess_chrges_value.setAttribute("class","excess_chrges")
+    let excess_km_price = document.createElement("p");
     excess_km_price.innerText = "Rs. "+ele.excess_km;
-    let km_limit_value = document.getElementById("km_limit_value");
+    let km_limit_value = document.createElement("p");
     km_limit_value.innerText = ele.km_limit+"/day";
     excess_chrges_value.append(excess_km_price,km_limit_value);
-
     row3_day_n_price_div.append(excess_chrges_keys,excess_chrges_value);
 
-    let btn=document.getElementById("Book_btn");
+    let btn=document.createElement("button");
+     btn.setAttribute("id","Book_btn");
     btn.innerText="BOOK NOW";
     btn.addEventListener("click",function (){
       addto_cart(data,ele.id);
     })
   
-    // box.append(img,name,price,btn);
-    //  price_details.append(row1_days,row1_day_n_price_div,row2_days,row2_day_n_price_div,row3_heading,row3_day_n_price_div);
-     box.append(name,img,availbilty_p,price_details);
+      price_details.append(row1_days,row1_day_n_price_div,row2_days,row2_day_n_price_div,row3_heading,row3_day_n_price_div);
+     box.append(name,img,availbilty_p,price_details,btn);
      contents.append(box);
   });
 }
 
 function addto_cart(arry,product_id){
-  cart_products=[{}];
-   cart_products=arry.filter((ele)=>{
-    return  ele.id == product_id ;
-  });
+  console.log("Add to payment ")
+  // cart_products=[{}];
+  //  cart_products=arry.filter((ele)=>{
+  //   return  ele.id == product_id ;
+  // });
   
-  bike_incart.push(cart_products[0]);
-  localStorage.setItem("cart",JSON.stringify(bike_incart));
-  localStorage.setItem("purchase",JSON.stringify(bike_incart));
+  // bike_incart.push(cart_products[0]);
+  // localStorage.setItem("cart",JSON.stringify(bike_incart));
+  // localStorage.setItem("purchase",JSON.stringify(bike_incart));
 }
 
 
